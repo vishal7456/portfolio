b32fdb3b7ced619355e1755ec7e8a0183f1f8485
vishal7456 <preetvishal7456@gmail.com>
2025-08-22 20:15:56 +0530

    Resume

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -314,7 +314,7 @@ const Contact: React.FC<ContactProps> = ({ email }) => {
             <h3 className="text-xl font-bold text-primary mb-4">My Resume</h3>
             <div className="relative" style={{ paddingTop: '56.25%' }}>
               <iframe
-                src="/CV_V.pdf"
+                src="/Resume.pdf"
                 className="absolute top-0 left-0 w-full h-full"
                 style={{ border: 'solid' }}
                 title="My Resume"
